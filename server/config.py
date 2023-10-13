9a8221f6b1b4128fe6e9f1ea32b3e6ff82b21003
Giselle <gisellec60@gmail.com>
2023-10-13 17:19:01 -0400

    remove templates

--- server/config.py
+++ server/config.py
@@ -5,7 +5,10 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(
     __name__,
@@ -14,7 +17,6 @@ app = Flask(
     template_folder='../client/build'
 )
 
-app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
