8a282e18f94fe684e55b653b527bcc2331ce4bb9
Giselle <gisellec60@gmail.com>
2023-12-06 17:54:01 -0500

    added some styling to the listing for dancers events

--- client/src/components/DancerEvent/ShowDancerEventsListing.js
+++ client/src/components/DancerEvent/ShowDancerEventsListing.js
@@ -7,18 +7,18 @@ function ShowDancerEventsListing({dancer, events, empty}) {
  
   
   const eventlist =
-    <Container fluid="md" className="list-container">
+    <Container fluid="md" className="containersize">
      {
        empty ? 
             <h3 className="nameHeading">{dancer['first']} {dancer['last']} : No Events Scheduled</h3>
           : <h3 className="nameHeading">{dancer['first']} {dancer['last']}  </h3>
      }
-        <Table responsive striped boarded variant="dark">
+        <Table responsive striped boarded variant="dark" className="text"  >
           <thead >
               <tr >
-                  <th>#</th>
+                  <th>Id</th>
                   <th>Date</th>
-                  <th>event Time</th>
+                  <th>Event Time</th>
                   <th>Arrival Time</th>
                   <th>Venue</th>
                   <th>Address</th>
